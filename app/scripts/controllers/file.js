753771c860a45a3a8deaa096b7c2d8c8b2e41970
unknown <568658857@qq.com>
2016-01-18 23:02:32 +0800

    upload file function

--- app/scripts/controllers/file.js
+++ app/scripts/controllers/file.js
@@ -8,18 +8,45 @@
  * Controller of the angularNewProjectApp
  */
 angular.module('angularNewProjectApp')
-    .controller('FileCtrl', ['fileService', function(fileService) {
-        var vm = this;
-        vm.file = null;
-        vm.errorMessage = null;
-        vm.upload = function() {
-            fileService.upload(vm.file).then(function(data) {
-                if (!data.status) {
-                    vm.errorMessage = 'upload failure!';
-                    return;
-                } else {
-                    vm.errorMessage = data.message;
+    // .controller('FileCtrl', ['fileService', '$scope', function(fileService, $scope) {
+    //     var vm = this;
+    //     vm.file = null;
+    //     vm.errorMessage = null;
+    //     vm.upload = function() {
+    //         fileService.upload(vm.file).then(function(data) {
+    //             if (!data.status) {
+    //                 vm.errorMessage = 'upload failure!';
+    //                 return;
+    //             } else {
+    //                 vm.errorMessage = data.message;
+    //             }
+    //         });
+    //     };
+    // }]);
+
+.controller('FileCtrl', ['$scope', 'Upload', '$timeout', function($scope, Upload, $timeout) {
+    $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'http://localhost:8080/files/upload',
+                data: {
+                    file: file
                 }
             });
-        };
-    }]);
+
+            file.upload.then(function(response) {
+                $timeout(function() {
+                    file.result = response.data;
+                });
+            }, function(response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function(evt) {
+                file.progress = Math.min(100, parseInt(100.0 *
+                    evt.loaded / evt.total));
+            });
+        }
+    }
+}]);
